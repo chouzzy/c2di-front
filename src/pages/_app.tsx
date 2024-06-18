89ab6d1bca6fd03b0095dd7b32a71578cd742568
Patricia Costa <patricosouza@hotmail.com>
2024-06-18 19:08:52 -0300

    add_gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraBaseProvider } from '@chakra-ui/react'
 import { fonts } from '../lib/fonts'
 import { theme } from '@/styles/theme'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +16,24 @@ export default function App({ Component, pageProps }: AppProps) {
           }
         `}
       </style>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11353857450"
+      ></Script>
+
+      <Script
+        id="google-tag-manager"
+        // strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-11353857450');
+         `,
+        }}
+      ></Script>
       <ChakraBaseProvider theme={theme}>
         <Head>
           <title>Natasha Macedo</title>
