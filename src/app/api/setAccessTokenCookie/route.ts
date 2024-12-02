ac142affb8be2337186bf8c0139e4a12c7ef0eb2
chouzzy <mathfernando@hotmail.com>
2024-12-02 14:37:46 -0300

    trycatch no setCookie

--- src/app/api/setAccessTokenCookie/route.ts
+++ src/app/api/setAccessTokenCookie/route.ts
@@ -8,36 +8,43 @@ import CryptoJS from 'crypto-js';
 
 const POST = withApiAuthRequired(async function POST(req) {
 
-    const res = new NextResponse();
-    const { accessToken } = await getAccessToken(req, res, {
-        authorizationParams: {
-            audience: process.env.AUTH0_AUDIENCE,
-            scope: "C2DI Auth API",
+    try {
+
+
+        const res = new NextResponse();
+        const { accessToken } = await getAccessToken(req, res, {
+            authorizationParams: {
+                audience: process.env.AUTH0_AUDIENCE,
+                scope: "C2DI Auth API",
+            }
+        })
+
+        if (!accessToken) {
+            throw Error("AccessToken not found")
         }
-    })
 
-    if (!accessToken) {
-        throw Error("AccessToken not found")
+        const secretKey = process.env.AUTH0_SECRET
+
+        if (!secretKey) {
+            throw Error('Secret key not found')
+        }
+        const encryptedToken = CryptoJS.AES.encrypt(accessToken, secretKey).toString();
+
+        cookies().set({
+            name: 'accessToken',
+            value: encryptedToken,
+            httpOnly: true,
+            domain: '.awer.co',
+            path: "/",
+            secure: true,
+            sameSite: 'lax'
+        })
+
+        return NextResponse.json({ message: "Cookies setados com sucesso!" });
+
+    } catch (error) {
+        throw error
     }
-
-    const secretKey = process.env.AUTH0_SECRET
-
-    if (!secretKey) {
-        throw Error ('Secret key not found')
-    }
-    const encryptedToken = CryptoJS.AES.encrypt(accessToken, secretKey).toString();
-
-    cookies().set({
-        name: 'accessToken',
-        value: encryptedToken,
-        httpOnly: true,
-        domain:'.awer.co',
-        path: "/",
-        secure: true,
-        sameSite: 'lax'
-    })
-
-    return NextResponse.json({ message: "Cookies setados com sucesso!" });
 
 
 })
